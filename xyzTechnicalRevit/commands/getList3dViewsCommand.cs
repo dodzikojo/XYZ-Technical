141106c029f7685eb1ebfc29f4942f6c885e4bd4
dodzikojo <dodzi@windowslive.com>
2023-04-07 08:44:35 +0100

    List all 3d views in revit

--- xyzTechnicalRevit/commands/getList3dViewsCommand.cs
+++ xyzTechnicalRevit/commands/getList3dViewsCommand.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace xyzTechnicalRevit.commands
 {
@@ -13,7 +15,41 @@ namespace xyzTechnicalRevit.commands
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+
+
+            ListAll3DViews(doc);
+
+
             return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Get all 3D views in the active project.
+        /// </summary>
+        /// <param name="doc"></param>
+        public void ListAll3DViews(Document doc)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(View3D));
+            List<View3D> views = collector.Cast<View3D>().ToList();
+
+            Window window = new Window();
+            window.Title = "List of 3D Views";
+            window.Width = 250;
+            window.Height = 300;
+
+            ListBox listBox = new ListBox();
+            listBox.ItemsSource = views;
+            listBox.DisplayMemberPath = "Name";
+
+            window.Content = listBox;
+            window.ShowDialog();
+
         }
     }
 }
